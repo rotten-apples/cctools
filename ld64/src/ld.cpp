49228e6e38ed599525622e5c7960eb572b2a9ea8
saurik <saurik@c21c009e-8f39-0410-915a-49ea6329ddb5>
2009-08-17 11:29:30 +0000

    Remove unneeded header to make ld64 compile on cygwin.
    git-svn-id: http://iphone-dev.googlecode.com/svn/branches/odcctools-9.2-ld@287 c21c009e-8f39-0410-915a-49ea6329ddb5

--- ld64/src/ld.cpp
+++ ld64/src/ld.cpp
@@ -31,7 +31,6 @@ extern "C" double log2 ( double ) throw();
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#include <sys/sysctl.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <limits.h>
